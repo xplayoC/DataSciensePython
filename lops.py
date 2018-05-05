3ce0ae36589cd21c4fa8995638ad49075c1533d2
Xplayo_Linux <jantonio9008@hotmail.com>
2018-05-05 16:47:10 -0700

    We generate a new script where we are probin the loops sentence in python.

---
+++ lops.py
@@ -0,0 +1,18 @@
+#In this scripts i will probe the loop senteces in python
+
+print("\n\nHere we are probing the while loop\n")
+
+i = 0
+while i < 10:
+	print("i has the value of:", i,  ".")
+	i = i + 1
+
+
+print("\n\nHere we are probing the for loop\n")
+
+for i in range(10, 0, -1):
+	print("i has the value of:", i,  ".")
+
+
+print("\nThe program has finished.")
+	
