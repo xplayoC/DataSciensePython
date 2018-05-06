dcd13075be7844f66b4f124af952f8abc31c8bec
Xplayo_Linux <jantonio9008@hotmail.com>
2018-05-05 17:03:59 -0700

    We have added a new while loop.

--- lops.py
+++ lops.py
@@ -14,5 +14,14 @@ for i in range(10, 0, -1):
 	print("i has the value of:", i,  ".")
 
 
+print("\n\nWe will print all the even numbers from 0 to 100")
+
+i = 0
+while i < 100:
+	if not(i % 2):
+		print(i)
+	i += 1
+
 print("\nThe program has finished.")
+
 	
